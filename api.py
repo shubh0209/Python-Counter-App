3bffe4474cb974d4092a01ee66aaf0268feb20b9
KUMAR ASHUTOSH <shubh0209>
2020-06-08 20:48:26 +0530

    added info api endpoint

--- api.py
+++ api.py
@@ -1,15 +1,25 @@
 from flask import Flask, json
+import git
 
 companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 count = 1
 
 api = Flask(__name__)
 
-@api.route('/counter', methods=['GET', 'PUT', 'DELETE'])
-def get_companies():
+@api.route('/counter', methods=['GET'])
+def get_counter():
   #count = count + 1
   #return json.dumps(companies)
-  return "<h1> Hi, Welcome </h1> <br> {}".format(count)
+  return "<h1> Hi, Welcome to counter</h1> <br> {}".format(count)
+
+@api.route('/info', methods=['GET'])
+def get_info():
+  #count = count + 1
+  #return json.dumps(companies)
+  repo = git.Repo(search_parent_directories=True)
+  sha = repo.head.object.hexsha
+  branch = repo.active_branch
+  return "<h1> Hi, Welcome to info </h1> <br/> Repo Name : {0} <br/> latest commit hash : {1} <br/> Active branch : {2}".format(repo,sha, branch)
 
 
 
